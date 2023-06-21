a76a9174356369b9825a9574e2050f5e940dfd6f
DanielAranedaM <d.aranedam@duocuc.cl>
2023-06-20 20:45:51 -0400

    nuevo proyecto

--- src/main/resources/public/js/formulario.js
+++ src/main/resources/public/js/formulario.js
@@ -1,4 +1,4 @@
-function validar(){
+function validar() {
     var nombreOK = false;
     var telefonoOK = false;
     var emailOK = false;
@@ -6,39 +6,39 @@ function validar(){
 
     var nombre = document.getElementById("nombre").value;
     var largoNombre = nombre.length
-    if(largoNombre > 3 && largoNombre < 20){
+    if (largoNombre > 3 && largoNombre < 20) {
         nombreOK = true
-    }else{
+    } else {
         alert("Nombre Incorrecto")
     }
 
     var telefono = document.getElementById("telefono").value;
     var largoTelefono = telefono.length
-    if(largoTelefono === 9){
+    if (largoTelefono === 9) {
         telefonoOK = true
-    }else{
+    } else {
         alert("Telefono Incorrecto")
     }
-                
- 
+
+
     var emailField = document.getElementById("email");
-    var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-    
-    if( validEmail.test(emailField.value) ){
-        emailOK = true        
-    }else{
+    var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+    if (validEmail.test(emailField.value)) {
+        emailOK = true
+    } else {
         alert('Email es invalido');
     }
-    
+
     var pass = document.getElementById("password").value;
     var largoPass = pass.length
-    if(largoPass > 5 && largoPass < 20){
+    if (largoPass > 5 && largoPass < 20) {
         passOK = true
-    }else{
+    } else {
         alert("Password Incorrecto")
     }
 
-    if(nombreOK && telefonoOK && emailOK && passOK){
+    if (nombreOK && telefonoOK && emailOK && passOK) {
         alert("Datos Registrados")
         window.location.href = "index.html";
     }
