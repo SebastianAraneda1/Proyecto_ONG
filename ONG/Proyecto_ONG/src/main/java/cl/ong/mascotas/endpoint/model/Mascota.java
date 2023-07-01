a60bbacdd8961b116aad1c703b4075fa3b1eb076
SebastianAraneda1 <sebastian_lb18@outlook.cl>
2023-07-01 17:24:33 -0400

    Añadir perros

--- ONG/Proyecto_ONG/src/main/java/cl/ong/mascotas/endpoint/model/Mascota.java
+++ ONG/Proyecto_ONG/src/main/java/cl/ong/mascotas/endpoint/model/Mascota.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Setter
 public class Mascota {
     private int id;
-    private int nroChip;
+    private int nro_Chip;
     private int edad;
     private String nombre;
     private String genero;
